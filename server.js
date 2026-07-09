78fdbdd3045979a941eabc9274e14219c04b78de
vidhikhedkar <vidhikhedkar7@gmail.com>
2026-07-09 15:36:06 +0530

    add

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 8080;
 
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Expenses Tracker API is running successfully 🚀",
+  });
+});
+
 
 app.listen(PORT, () => {
   console.log(`🚀 API Server running on port ${PORT}`);
