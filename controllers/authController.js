dcf74ed9ac4cba033667f801ec4ba9b702596bac
vidhikhedkar <vidhikhedkar7@gmail.com>
2026-07-10 12:42:58 +0530

    add

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,9 +13,9 @@ const generateToken = (id) => {
 // Cookie Options
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
-  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+  secure: true,
+  sameSite: "none",
+  maxAge: 30 * 24 * 60 * 60 * 1000,
 };
 
 // =========================
